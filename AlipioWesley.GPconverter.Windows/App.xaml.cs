2c0e38cf5e5b6bc012423c81bf5e9df8a3f1a4c9
StarlightBreaker <alipiojanwesley@gmail.com>
2019-09-13 18:47:55 +0800

    GP_Converter

---
+++ AlipioWesley.GPconverter.Windows/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AlipioWesley.GPconverter.Windows
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
